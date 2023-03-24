f37ba6e88c2bb66e37bf037eaa39de3b776eb9ba
Agknaton Bottenberg <22169423+agknaton@users.noreply.github.com>
2023-03-24 08:52:06 +0100

    Adding todo.txt install

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ case $OS in
 			PKG_UPDATE="sudo apt-get update"
 			PKG_UPGRADE="sudo apt-get upgrade"
 			PKG_INSTALL="sudo apt-get install"
+			TARGET_SYSTEM="debian"
 		elif [ $(dpkg-query -W -f='${Status}' pacman 2>/dev/null | grep -c "ok installed") -eq 1 ]
 		then
 			PKG_CMD="pacman -S"
@@ -68,13 +69,31 @@ Install ()
 
 	echo "Run pre-install commands"
 	# nodejs is required for GitHub Copilot
-	curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&sudo apt-get install -y nodejs
+	#curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&sudo apt-get install -y nodejs
+	Run curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash -
+	Run sudo apt-get install -y nodejs
+	# Create support folders
+	Run mkdir -p ~/apps_data
 
 	echo "Install system packages"
 	Run $PKG_INSTALL $(xargs echo <pkgs_install.txt)
 
 	echo "Install python packages"
 	Run pip install -r pip_pkgs.txt
+
+	echo "Install todotxt-cli"
+	if [ $TARGET_SYSTEM == "debian" ]; then
+		Run git clone https://github.com/todotxt/todo.txt-cli.git
+		Run cd todo.txt-cli
+		Run mkdir -p temp
+		Run make
+		Run sudo make install CONFIG_DIR=temp INSTALL_DIR=/usr/bin BASH_COMPLETION=/usr/share/bash-completion/completions
+		Run cd ..
+		Run sudo rm -rf todo.txt-cli
+		Run sudo rm -rf .todo-txt
+	else
+		Run $PKG_CMD todotxt-cli
+	fi
 }
 
 
